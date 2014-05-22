f21d3595aa7708dac1b5995eb9bdf82e930fb85e
António Lopes <antonio@alopes.co.uk>
2014-05-22 01:19:21 +0100

    Improved the CSS and added a readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Angular.js sandbox
+
+Playing with angular.js while trying to create a music player.
+
+If you fell like checking it out, just run:
+`npm install` followed by `grunt serve`
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -67,16 +67,17 @@ app.controller('PlayerCtrl', ['$scope', 'player', 'msgBus', function ($scope, pl
     // console.log(event);
     // console.log(data);
     $scope.isActive = true;
-    $scope.song.title = data.song.title;
+    $scope.song.title = 'Title: ' + data.song.title;
+    $scope.song.cover = data.song.cover;
 
   }, $scope);
 
 }]);
 
 app.songs = [
-  {title:'foobar'},
-  {title:'lolwat'},
-  {title:'dfsfdsfds'},
-  {title:'fhtrh'},
-  {title:'5ghg'},
+  {title:'foobar', cover: '//i1.sndcdn.com/artworks-000080064064-70urpi-t500x500.jpg'},
+  {title:'lolwat', cover: '//i1.sndcdn.com/artworks-000079955535-ev0g3t-t500x500.jpg?30a2558'},
+  {title:'dfsfdsfds', cover: '//i1.sndcdn.com/artworks-000079282662-cctjup-t500x500.jpg?30a2558'},
+  {title:'fhtrh', cover: '//i1.sndcdn.com/artworks-000079521520-z4xodd-t500x500.jpg?30a2558'},
+  {title:'5ghg', cover: '//i1.sndcdn.com/artworks-000079522528-q7gq4p-t500x500.jpg?30a2558'},
 ];
